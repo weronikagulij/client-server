f13f7d6a0576639370a331f578b04cb4c409d29a
Weronika Jankowska <weronika.jankowska098@gmail.com>
2019-03-03 17:31:15 +0100

    Update makefile.sh

--- makefile.sh
+++ makefile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-gcc ./inf136720_c.c -o ./client
-gcc ./inf136720_s.c -o ./server
+gcc ./client.c -o ./client
+gcc ./server.c -o ./server
 gnome-terminal -e "./server"
 gnome-terminal -e "./client"
 gnome-terminal -e "./client"
